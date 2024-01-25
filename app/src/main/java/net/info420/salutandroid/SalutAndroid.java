ba13a4b5e512fddf8ab5f296162f747d0a311ffa
Nicholas Paquet <nicholas.paquet.rowsell@cegepsi.ca>
2024-01-25 14:45:56 -0500

    Travail sur orientation et langue

---
+++ app/src/main/java/net/info420/salutandroid/SalutAndroid.java
@@ -0,0 +1,14 @@
+package net.info420.salutandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SalutAndroid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.salut_android_layout);
+    }
+}
