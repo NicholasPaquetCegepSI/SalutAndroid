0fd6e3737185733dd421ae66a1f0acee8800d851
Nicholas Paquet <nicholas.paquet.rowsell@cegepsi.ca>
2024-01-29 16:17:15 -0500

    Correction d'une coquille

--- app/src/main/java/net/info420/salutandroid/SalutAndroid.java
+++ app/src/main/java/net/info420/salutandroid/SalutAndroid.java
@@ -10,5 +10,6 @@ public class SalutAndroid extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.salut_android_layout);
+
     }
 }
